e9b0c46083f6f2fb6d0828d25dcc09edf12a1ea0
Derik Rocha <122660784+yHeyDark@users.noreply.github.com>
2025-05-18 19:19:49 -0300

    fluxogramadaengenharia

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿string resposta;
+Console.WriteLine("Está se movendo? ");
+resposta = Console.ReadLine()!.ToUpper();
+if (resposta == "SIM")
+{
+    Console.WriteLine("Deveria: ");
+    resposta = Console.ReadLine()!.ToUpper();
+
+    if (resposta == "SIM")
+
+    {
+        Console.WriteLine("otimo");
+    }
+    else
+    {
+        Console.WriteLine("Silver Tape");
+    }
+
+}
+
+else
+{
+    Console.WriteLine("Deveria ? ");
+    resposta = Console.ReadLine()!.ToUpper();
+    if (resposta == "SIM")
+    {
+        Console.WriteLine("WD-40");
+    }
+    else
+    {
+        Console.WriteLine("otimo");
+    }
+    
+}
